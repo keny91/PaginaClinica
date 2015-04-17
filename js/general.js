bce3b903b67e556b58d0c008f0bae4fd39577abd
keny91 <luishmanas@gmail.com>
2015-04-17 10:19:27 +0200

    Revert "Revert "que cambie?""
    This reverts commit 752ececd7fa3d99987d4fd75a37815eb95c9edf4.

--- js/general.js
+++ js/general.js
@@ -12,6 +12,7 @@
       map: map,
       title: 'Centro médico Concha Espina'});
   }
+  
   //map = new google.maps.Map(document.getElementById("map_canvas"), { mapTypeId: google.maps.MapTypeId.ROADMAP });
   google.maps.event.addDomListener(window, 'load', initialize);
   //google.maps.event.trigger(map, 'resize');
@@ -98,7 +99,7 @@ function clickContacto(){
 
   document.getElementById("map-canvas").style.display="block";
   initialize();
-  
+ /* google.maps.event.addDomListener(window, 'load', initialize);*/
 }
 
 
