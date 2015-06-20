0aff899fb0d14d978e18940c249dc3982ccf3101
keny91 <luishmanas@gmail.com>
2015-06-20 14:13:14 +0200

    twitter winget
    also some new experiments

--- js/general.js
+++ js/general.js
@@ -116,11 +116,33 @@ function clickContacto(){
 }
 
 
+
+
+
+
+
 /*load each HTML*/
 function load_Home(){
 
   $("#contenedor_bloques").load("./html/home.html");
   clickInicio();
+
+  window.twttr = (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0],
+                t = window.twttr || {};
+              if (d.getElementById(id)) return t;
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "https://platform.twitter.com/widgets.js";
+              fjs.parentNode.insertBefore(js, fjs);
+             
+              t._e = [];
+              t.ready = function(f) {
+                t._e.push(f);
+              };
+             
+              return t;
+            }(document, "script", "twitter-wjs"));
 }
 
 
